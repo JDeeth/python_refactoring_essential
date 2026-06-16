df21ebe6a27fec051a4978fcf11c68e50bcefcb2
Jack Deeth <jack.deeth@theaccessgroup.com>
2026-06-16 19:24:00 +0100

    It works!

--- comments/src/X1.py
+++ comments/src/X1.py
@@ -1,4 +1,4 @@
-class X1:
+class Maths:
 
     @staticmethod
     def sum_squares_of_range(lower_bound, upper_bound):
@@ -6,7 +6,7 @@ class X1:
 
         for i in range(lower_bound, upper_bound + 1):
             # Add square of each number in the range
-            accumulated_sum += X1.square(i)
+            accumulated_sum += Maths.square(i)
 
         return accumulated_sum
 
--- comments/test/test_X1.py
+++ comments/test/test_X1.py
@@ -1,29 +1,29 @@
 import unittest
 
-from comments.src.X1 import X1
+from comments.src.X1 import Maths
 
 
 class TestX1(unittest.TestCase):
 
     def test_n_basic(self):
-        self.assertEqual(X1.square(0), 0)
-        self.assertEqual(X1.square(2), 4)
-        self.assertEqual(X1.square(-3), 9)
+        self.assertEqual(Maths.square(0), 0)
+        self.assertEqual(Maths.square(2), 4)
+        self.assertEqual(Maths.square(-3), 9)
 
     def test_m_single_value(self):
-        self.assertEqual(X1.sum_squares_of_range(3, 3), 9)
+        self.assertEqual(Maths.sum_squares_of_range(3, 3), 9)
 
     def test_m_small_range(self):
-        self.assertEqual(X1.sum_squares_of_range(1, 3), 14)
+        self.assertEqual(Maths.sum_squares_of_range(1, 3), 14)
 
     def test_m_larger_range(self):
-        self.assertEqual(X1.sum_squares_of_range(0, 3), 14)
+        self.assertEqual(Maths.sum_squares_of_range(0, 3), 14)
 
     def test_m_negative_range(self):
-        self.assertEqual(X1.sum_squares_of_range(-2, 0), 5)
+        self.assertEqual(Maths.sum_squares_of_range(-2, 0), 5)
 
     def test_m_larger_negative_to_positive(self):
-        self.assertEqual(X1.sum_squares_of_range(-1, 1), 2)
+        self.assertEqual(Maths.sum_squares_of_range(-1, 1), 2)
 
 
 if __name__ == "__main__":
